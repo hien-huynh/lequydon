267833dc284953c86e4f32ad36eecdc76d5a4f9d
hien-huynh <hien37211@gmail.com>
2026-07-06 01:32:38 +0700

    add folder uploads

---
+++ wp-content/uploads/aios/firewall-rules/settings.php
@@ -0,0 +1,6 @@
+<?php __halt_compiler();
+/**
+ * This file was created by All In One Security (AIOS) plugin.
+ * The file is required for storing and retrieving your firewall's settings.
+ */
+{"aios_enable_rename_login_page":"","aios_login_page_slug":"","aios_enable_brute_force_attack_prevention":"","aios_brute_force_secret_word":"","aios_cookie_based_brute_force_redirect_url":"http:\/\/127.0.0.1","aios_brute_force_attack_prevention_pw_protected_exception":"","aios_brute_force_attack_prevention_ajax_exception":"","aios_brute_force_secret_cookie_name":"aios_brute_force_secret_f131eb49a9d26ada036228bf64857815","aios_ip_retrieve_method":"","aiowps_ip_retrieve_method":"","aiowps_blacklist_ips":[],"aiowps_blacklist_user_agents":[],"aiowps_block_fake_googlebots":false}
